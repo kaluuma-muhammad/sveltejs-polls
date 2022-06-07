fbdab0c6528d6761dc56b3634e420141a044e11a
Meddy <kaluumamhd@gmail.com>
2022-06-07 16:48:48 +0300

    Header and Footer module

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	let head = 'voters';
+	import Header from "./components/Header.svelte";
+	import Footer from "./components/Footer.svelte";
 </script>
 
+<Header />
 <main>
-	<h1>Hello {head}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+		Iste, deserunt. Est minus inventore fuga eius, 
+		impedit iusto ipsum cum qui molestiae tenetur id hic esse blanditiis sunt sint totam aperiam!
+	</p>
 </main>
-
+<Footer />
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		max-width: 960px;
+		margin: 40px auto;
 	}
 </style>
